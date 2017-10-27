f3bf1de74cdbc9e8790e6fb66e201b6cca702ef2
Hervé Droit <herve.drt@gmail.com>
2017-10-27 02:21:20 +0200

    Updated SEO tags. Dealing with project illustrations.

--- app/scenes/App/index.jsx
+++ app/scenes/App/index.jsx
@@ -16,31 +16,31 @@ class App extends React.Component {
                 <link rel="icon" type="image/png" href="assets/vectors/favicon/favicon-16x16.png" sizes="16x16" />
                 <link rel="manifest" href="assets/vectors/favicon/manifest.json" />
                 <link rel="shortcut icon" href="assets/vectors/favicon/favicon.ico" />
-                <meta name="msapplication-config" content="assets/vectors/favicon/browserconfig.xml" />
+                <meta name="msapplication-config" content="assets/SEO/preview.jpg" />
                 <meta name="theme-color" content="#00aeef" />
-                <meta name="description" content="Spotmyflat, 1ère source d'informations off-market. Inscription gratuite et réservée aux professionnels de l’immobilier." />
-                <meta name="keywords" content="spotmyflat immobilier professionnel off market off-market outil productivité campagnes emailing agent agents chasseur chasseurs off-market vente achat transactions" />
+                <meta name="description" content="" />
+                <meta name="keywords" content="herve droit software engineer tech ux developer" />
                 <meta name="author" content="Spotmyflat" />
                 <meta name="copyright" content="Spotmyflat 2016" />
                 <meta name="application-name" content="Spotmyflat" />
                 <meta property="fb:page_id" content="1664299847228782" />
                 <meta property="og:site_name" content="Spotmyflat" />
                 <meta property="og:locale" content="fr_FR" />
-                <meta property="og:title" content="Spotmyflat | 1ère source d'informations off-market" />
+                <meta property="og:title" content="Hervé Droit - Software Engineer - Side Experiences" />
                 <meta property="og:type" content="article" />
-                <meta property="og:image" content="https://s3-eu-west-1.amazonaws.com/assets.spotmyflat.com/public/shared-assets/smf-sharingcover-181016.png" />
-                <meta property="og:image:type" content="image/png" />
+                <meta property="og:image" content="http://www.hervedroit.com/assets/SEO/preview.jpg" />
+                <meta property="og:image:type" content="image/jpg" />
                 <meta property="og:image:width" content="1125" />
                 <meta property="og:image:height" content="675" />
-                <meta property="og:url" content="https://www.spotmyflat.com" />
-                <meta property="og:description" content="1ère source d'informations off-market. Inscription gratuite et réservée aux professionnels de l’immobilier." />
+                <meta property="og:url" content="http://www.hervedroit.com" />
+                <meta property="og:description" content="I'm Hervé, 24 yo. My experience is a bit unusual, as I always loved to test and create new things. Browse a few side experiences I have." />
                 <meta property="twitter:card" content="summary" />
-                <meta property="twitter:site" content="@spotmyflat" />
-                <meta property="twitter:creator" content="@spotmyflat" />
-                <meta name="twitter:title" content="Spotmyflat | 1ère source d'informations off-market" />
-                <meta name="twitter:text:description" content="1ère source d'informations off-market. Inscription gratuite et réservée aux professionnels de l’immobilier." />
-                <meta name="twitter:description" content="Spotmyflat, communiquez différemment sur le off-market. Inscription gratuite et réservée aux professionnels de l’immobilier." />
-                <meta name="twitter:image" content="https://s3-eu-west-1.amazonaws.com/assets.spotmyflat.com/public/shared-assets/smf-sharingcover-181016.png" />
+                <meta property="twitter:site" content="@hervedroit" />
+                <meta property="twitter:creator" content="@hervedroit" />
+                <meta name="twitter:title" content="Hervé Droit - Software Engineer - Side Experiences" />
+                <meta name="twitter:text:description" content="I'm Hervé, 24 yo. My experience is a bit unusual, as I always loved to test and create new things. Browse a few side experiences I have." />
+                <meta name="twitter:description" content="I'm Hervé, 24 yo. My experience is a bit unusual, as I always loved to test and create new things. Browse a few side experiences I have." />
+                <meta name="twitter:image" content="http://www.hervedroit.com/assets/SEO/preview.jpg" />
             </MetaTags>
             <Switch>
                 <Route
